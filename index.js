d6b9251a6e2fc80a1e9b04fd292ddb4ef16aa531
dilkush <dilkushrazz604@gmail.com>
2026-02-18 01:51:45 +0530

    added functionalities like get , post , and middlewares

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const http = require("http");
 
 const server = http.createServer((req, res) => {
@@ -8,3 +9,102 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+=======
+const http=require('http');
+const fs=require('fs');
+const path = require('path');
+class neuroninjas{// constrcutor mai joh hi store karna ho use banate hai this. karke taaki baad mai use kar sake
+    constructor(){
+        this.routes=[];
+        this.middlewares=[];
+
+    };
+
+    get(path,handler){
+        let route={
+            method:'GET',
+            path:path,
+            handler:handler
+        }
+        this.routes.push(route)
+    };
+    use(middleware){
+        this.middlewareS.push(middleware)
+    }
+    post(Path,handler){
+        let route={
+            method:'POST',   
+            path:path,
+            handler:handler}
+        this.routes.push(route)
+    };
+
+    listen(port,call){
+        const server=http.createServer((req,res)=>{
+            this.routes.find(route=>route.method===req.method && route.path===req.url)
+                const route=this.routes.find(route=>route.method===req.method && route.path===req.url);
+            const i=0;
+            function next(){ 
+
+                if(i<this.middlewares.length){
+                    this.middlewares[i](req,new response(res),next);
+                    i++;
+                }
+                else{
+                    if(route){
+                        route.handler(req,new response(res))
+                    }
+                    else{
+                        res.writeHead(404);
+                        res.end('not found')
+                    }
+                }
+            }
+    next();
+                
+        })
+        server.listen(port,call);
+    };
+    
+}
+
+
+
+
+class response {
+    constructor(res){
+        this.res=res;
+    }
+    send(data){
+        this.res.end(data);
+
+    }
+    json(data){
+        this.res.setHeader('Content-Type','application/json');
+        this.res.end(JSON.stringify(data)); // it is used to convert a javascript object into a json string
+
+    }
+    render(view){
+        const filename=path.join(__dirname,view+'.html');
+        fs.readFile(filename,(err,data)=>{
+            if(err){
+                this.res.writeHead(500);
+                this.res.end("Internal Server Error");
+            }
+            else{
+                this.res.setHeader('Content-Type','text/html');
+                this.res.end(data);
+            }   
+    }
+    )}}
+let dilkush =new neuroninjas();
+dilkush.get('/',(req,res)=>{
+    res.send('hello')
+})
+dilkush.get('/about',(req,res)=>{
+    res.render("index")
+})
+dilkush.listen(3000,()=>{
+    console.log('server is running at port 3000')
+})
+>>>>>>> 2f732f7 (added functionalities like get , post , and middlewares)
---
+++ readme.md
@@ -0,0 +1,21 @@
+// --------------------Main idea:- creating an express-like app named neuroninjas 
+//breaking the promblem in various steps:-
+// neuroninjas.get()
+neuroninjas.post()
+neuroninjas.listen()
+neuroninjas.use()
+
+**************thsese all functionalities should work 
+
+architecture :- 
+1.http server is created 
+2.http request 
+3. middleware is integrated ---> request aur response ka beech mai hota hai aur next ko  transfer kar deta hai 
+4. route is defined 
+5. handler is defined 
+6. response is send
+
+
+//------------first create a main class neuroninjas 
+then various methods like get ,post ,listen will be created 
+
